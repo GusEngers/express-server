8ef64c3aa8b7db957825030171694acd7dca39d5
GusEngers <gustavoengers6441@gmail.com>
2024-01-06 17:16:59 -0300

    Páginas para home, error y not found page

--- src/client/controllers/index.js
+++ src/client/controllers/index.js
@@ -1,3 +1,11 @@
 // CONTROLADORES DE RUTAS
 
-module.exports = {};
+module.exports = {
+  homePage: (req, res, next) => {
+    try {
+      res.render('pages/home', { title: 'Express-Server' });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
